e9c96d575db98a222bc51a27095129ef25bdd790
AutomaticOrca <liangjiaying1013@gmail.com>
2024-09-21 21:15:50 +1000

    apiconfig change to super long url

--- src/shared/util/apiConfig.js
+++ src/shared/util/apiConfig.js
@@ -1,6 +1,6 @@
 const API_URL =
   import.meta.env === "development"
     ? "http://localhost:5005/api"
-    : "http://13.54.7.53:5005/api";
+    : "http://ec2-13-54-7-53.ap-southeast-2.compute.amazonaws.com:5005/api";
 
 export default API_URL;
