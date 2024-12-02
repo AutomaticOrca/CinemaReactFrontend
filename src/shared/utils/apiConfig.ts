c4a33deb095fbc4efb65b2a4ce0fe30dfda4b173
AutomaticOrca <liangjiaying1013@gmail.com>
2024-12-02 18:11:59 +1100

    api url changed to vercel

--- src/shared/utils/apiConfig.ts
+++ src/shared/utils/apiConfig.ts
@@ -1,6 +1,6 @@
 const API_URL =
   import.meta.env.VITE_APP_NODE_ENV === "development"
     ? "http://localhost:5005/api"
-    : "http://ec2-13-54-7-53.ap-southeast-2.compute.amazonaws.com:5005/api";
+    : "https://cinema-mern-backend.vercel.app/api/cinemas";
 
 export default API_URL;
