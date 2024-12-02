5828d48759a396cebf31bddc95ba624ec6722933
AutomaticOrca <liangjiaying1013@gmail.com>
2024-12-02 18:14:03 +1100

    api url bug fix

--- src/shared/utils/apiConfig.ts
+++ src/shared/utils/apiConfig.ts
@@ -1,6 +1,6 @@
 const API_URL =
   import.meta.env.VITE_APP_NODE_ENV === "development"
     ? "http://localhost:5005/api"
-    : "https://cinema-mern-backend.vercel.app/api/cinemas";
+    : "https://cinema-mern-backend.vercel.app/api";
 
 export default API_URL;
